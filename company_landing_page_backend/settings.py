1991a1b3aac38c280a60cd6dfb7b6e23e80edcb2
Milad Khnefes <milad.khnefes.16@gmail.com>
2026-04-13 10:52:42 +0300

    Allowed all CORS for public deployment

--- company_landing_page_backend/settings.py
+++ company_landing_page_backend/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS Settings
 CORS_ALLOWED_ORIGINS = env.list(
     'CORS_ALLOWED_ORIGINS',
-    default=['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173', 'http://127.0.0.1:5173']
+    default=['*'],
 )
 
 # Django REST Framework Settings
